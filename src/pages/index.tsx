7eea9d698f07ae8222f01cbc577e25f382d9196a
hota1024 <hotalog@hota1024.com>
2021-07-02 09:56:00 +0900

    :tada: init

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next'
+import Head from 'next/head'
+
+/**
+ * HomePage component.
+ */
+export const HomePage: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>home - sh-pc-part-vote-ui</title>
+      </Head>
+
+      <div>sh-pc-part-vote-ui</div>
+    </>
+  )
+}
+
+export default HomePage
