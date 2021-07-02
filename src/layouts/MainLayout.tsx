93f344549224bb62083b74be6c6af1a6a3032afd
hota1024 <hotalog@hota1024.com>
2021-07-02 18:20:38 +0900

    💄 add logged in butons

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,5 +1,7 @@
 import { Container, AppBar, Toolbar, Button, Box } from '@material-ui/core'
+import { useAtom } from 'jotai'
 import Link from 'next/link'
+import { jwtAtom, userAtom } from '../store/jwt'
 
 /**
  * MainLayout props.
@@ -12,6 +14,14 @@ export type MainLayoutProps = {
  * MainLayout component.
  */
 export const MainLayout: React.VFC<MainLayoutProps> = (props) => {
+  const [jwt, setJwt] = useAtom(jwtAtom)
+  const [, setUser] = useAtom(userAtom)
+
+  const logout = () => {
+    setJwt(null)
+    setUser(null)
+  }
+
   return (
     <>
       <AppBar position="static">
@@ -20,13 +30,38 @@ export const MainLayout: React.VFC<MainLayoutProps> = (props) => {
             <Button color="inherit">PCパーツ総選挙 テスト版</Button>
           </Link>
           <div style={{ flexGrow: 1 }}></div>
-          <Button color="inherit" variant="text" disableElevation>
-            サインイン
-          </Button>
-          <div style={{ padding: '0 8px' }}></div>
-          <Button color="inherit" variant="text" disableElevation>
-            サインアップ
-          </Button>
+          {jwt ? (
+            <>
+              <Link href="/profile">
+                <Button color="inherit" variant="text" disableElevation>
+                  マイページ
+                </Button>
+              </Link>
+              <div style={{ padding: '0 8px' }}></div>
+              <Button
+                color="secondary"
+                variant="text"
+                disableElevation
+                onClick={logout}
+              >
+                ログアウト
+              </Button>
+            </>
+          ) : (
+            <>
+              <Link href="/signin">
+                <Button color="inherit" variant="text" disableElevation>
+                  サインイン
+                </Button>
+              </Link>
+              <div style={{ padding: '0 8px' }}></div>
+              <Link href="/signup">
+                <Button color="inherit" variant="text" disableElevation>
+                  サインアップ
+                </Button>
+              </Link>
+            </>
+          )}
         </Toolbar>
       </AppBar>
 
